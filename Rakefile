ddb2faecbcb318199657c1d262f88d79bfb64b19
AJ Guiles <ashiguiles@gmail.com>
2019-06-28 03:04:27 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,6 @@
+
+# to load up our environmen
+require './config/environment'
+
+# to get Rake tasks from the sinatra-activerecord gem.
+require 'sinatra/activerecord/rake'
---
+++ db/migrate/20190628024202_create_dogs.rb
@@ -0,0 +1,22 @@
+# IMPORTANT-specify the version of ActiveRecord when creatingMigration per below.
+class CreateDogs < ActiveRecord::Migration[5.2]
+  
+#   def up
+#     create_table :dogs do |t|
+#       t.string :name
+#       t.string :breed
+#   end
+# end
+ 
+#   def down
+#     drop_table :dogs
+#   end
+
+ def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+end
